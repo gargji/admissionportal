ee1d62115c8fcc24ca755a54ce9a40edbb1edcd7
gargji <gargjirahul@gmail.com>
2023-02-02 15:14:59 +0530

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 
-const connectDB=require('./db/connectDB.JS')
+const connectDB=require('./db/connectDB')
 connectDB()
 const flash = require("connect-flash");
 const session =require('express-session')
